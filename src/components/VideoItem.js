32eff2ee08093cc2ec75c2122d2ef27f5112c87b
Justin Chau <chausicle@gmail.com>
2020-06-25 18:32:42 -0700

    Add style to VideoItem and initialize VideoDetail

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,13 @@
+import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} />
-      {video.snippet.title}
+    <div className="video-item item" onClick={(e) => onVideoSelect(video)}>
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title.toString()}</div>
+      </div>
     </div>
   );
 };

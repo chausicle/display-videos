12440810b207668935546369d38f5fef4caeab7d
Justin Chau <chausicle@gmail.com>
2020-06-25 17:33:18 -0700

    User can search with SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+  state = {
+    videos: [],
+  };
+
+  onTermSubmit = async (term) => {
+    const KEY = "AIzaSyDdHw0YgHdNXIyeplj1yYw-JnxDZjhqCcE";
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: `${KEY}`,
+      },
+    });
+
+    console.log(response.data.items);
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onTermSubmit={this.onTermSubmit} />
       </div>
     );
   }
